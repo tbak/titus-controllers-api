1fc91df8209e229d0f2e8703ad2e250f39aff8d7
Keyur Joshi <kjoshi@netflix.com>
2020-09-01 15:32:46 -0700

    Adding CapacityGroup types

---
+++ api/resourcepool/v1/capacitygroup_types.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2020 Netflix, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package v1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +kubebuilder:object:root=true
+
+type CapacityGroup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata.omitempty"`
+
+	Spec CapacityGroupSpec `json:"spec,omitempty"`
+}
+
+type CapacityGroupSpec struct {
+	CapacityGroupName string `json:"capacityGroupName"`
+	ResourcePoolName  string `json:"resourcePoolName"`
+	SchedulerName     string `json:"schedulerName"`
+	InstanceCount     uint32 `json:"instanceCount"`
+	ComputeResource   `json:"resourceDimensions"`
+}
+
+// +kubebuilder:object:root=true
+
+type CapacityGroupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []CapacityGroup `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CapacityGroup{}, &CapacityGroupList{})
+}
